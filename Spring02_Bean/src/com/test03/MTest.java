68b2f49abffbf53e28f7491e59be9f65b004f8dd
Kweon <su539@naver.com>
2024-10-22 11:50:15 +0900

    Framework_Spring_Bean

--- Spring02_Bean/src/com/test03/MTest.java
+++ Spring02_Bean/src/com/test03/MTest.java
@@ -1,18 +1,19 @@
-package com.test02;
+package com.test03;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MTest {
+
 	public static void main(String[] args) {
 		ApplicationContext factory =
-				new ClassPathXmlApplicationContext("com/test02/applicationContext.xml");
+				new ClassPathXmlApplicationContext("com/test03/applicationContext.xml");
 		
 		Address kim = (Address)factory.getBean("kim");
 		System.out.println(kim);
 		
 		Address hong = (Address)factory.getBean("hong");
 		System.out.println(hong);
-		
 	}
+
 }
