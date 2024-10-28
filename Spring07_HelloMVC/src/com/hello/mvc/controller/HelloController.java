7e41fab57cbfba6597ee03ba720e55fbc06d2d82
Kweon <su539@naver.com>
2024-10-28 17:20:24 +0900

    Framework_Spring_HelloMVC

--- Spring07_HelloMVC/src/com/hello/mvc/controller/HelloController.java
+++ Spring07_HelloMVC/src/com/hello/mvc/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hello.mvc.model.HelloBiz;
 
@@ -19,6 +21,16 @@ public class HelloController {
 		model.addAttribute("msg",biz.getHello());
 		
 		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/bye.do")
+	public ModelAndView getBye(@RequestParam("name")String nickName) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("/WEB-INF/views/bye.jsp");
+		mav.addObject("msg","bye, "+nickName);
+		
+		return mav;
 	}
 	
 }
