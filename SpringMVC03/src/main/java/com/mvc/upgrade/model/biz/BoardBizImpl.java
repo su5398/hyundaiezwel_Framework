1b775b6aafb9d6e96e6bf2a2a49b61847530d5bb
Kweon <su539@naver.com>
2024-10-29 16:40:02 +0900

    Framework_Spring_MVC03

---
+++ SpringMVC03/src/main/java/com/mvc/upgrade/model/biz/BoardBizImpl.java
@@ -0,0 +1,34 @@
+package com.mvc.upgrade.model.biz;
+
+import java.util.List;
+
+import com.mvc.upgrade.model.dto.BoardDto;
+
+public class BoardBizImpl implements BoardBiz{
+
+	@Override
+	public List<BoardDto> selectList() {
+		return null;
+	}
+
+	@Override
+	public BoardDto selectOne(int myno) {
+		return null;
+	}
+
+	@Override
+	public int insert(BoardDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int update(BoardDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int delete(int myno) {
+		return 0;
+	}
+
+}
---
+++ SpringMVC03/src/main/java/com/mvc/upgrade/model/dao/BoardDaoImpl.java
@@ -0,0 +1,34 @@
+package com.mvc.upgrade.model.dao;
+
+import java.util.List;
+
+import com.mvc.upgrade.model.dto.BoardDto;
+
+public class BoardDaoImpl implements BoardDao{
+
+	@Override
+	public List<BoardDto> selectList() {
+		return null;
+	}
+
+	@Override
+	public BoardDto selectOne(int myno) {
+		return null;
+	}
+
+	@Override
+	public int insert(BoardDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int upgrade(BoardDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int delete(int myno) {
+		return 0;
+	}
+
+}
