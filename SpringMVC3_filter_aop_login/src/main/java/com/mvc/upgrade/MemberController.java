a2579de36b84f3342f264308e09bf8920d5bc5cd
Kweon <su539@naver.com>
2024-11-01 11:48:21 +0900

    Framework_Spring_MVC03_filter_aop_login

---
+++ SpringMVC3_filter_aop_login/src/main/java/com/mvc/upgrade/MemberController.java
@@ -0,0 +1,36 @@
+package com.mvc.upgrade;
+
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mvc.upgrade.model.biz.MemberBiz;
+import com.mvc.upgrade.model.dto.MemberDto;
+
+@Controller
+public class MemberController {
+	
+	private Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@Autowired
+	private MemberBiz biz;
+	
+	@RequestMapping("/loginform.do")
+	public String loginForm() {
+		logger.info("LOGIN FORM");
+		return "mvclogin";
+	}
+	
+	@RequestMapping("/ajaxlogin.do")
+	public Map<String, Boolean> ajaxLogin(@RequestBody MemberDto dto) {
+		logger.info("LOGIN");
+		System.out.println(dto.getMemberid());
+		System.out.println(dto.getMemberpw());
+		return null;
+	}
+}
---
+++ SpringMVC3_filter_aop_login/src/main/java/com/mvc/upgrade/model/dao/MemberDaoImpl.java
@@ -0,0 +1,20 @@
+package com.mvc.upgrade.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.mvc.upgrade.model.dto.MemberDto;
+
+@Repository
+public class MemberDaoImpl implements MemberDao{
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Override
+	public MemberDto login(MemberDto dto) {
+		
+		return null;
+	}
+}
