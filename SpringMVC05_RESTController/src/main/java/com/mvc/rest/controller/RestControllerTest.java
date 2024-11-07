a6f76d5d4c0901db4235d61eda22aa1f36eb4628
Kweon <su539@naver.com>
2024-11-07 11:44:57 +0900

    Framework_Spring_MVC05_RESTController

--- SpringMVC05_RESTController/src/main/java/com/mvc/rest/controller/RestControllerTest.java
+++ SpringMVC05_RESTController/src/main/java/com/mvc/rest/controller/RestControllerTest.java
@@ -1,11 +1,12 @@
 package com.mvc.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.mvc.rest.model.biz.RestBiz;
 
-@org.springframework.web.bind.annotation.RestController
-public class RestController {
+@RestController
+public class RestControllerTest {
 	
 	@Autowired
 	private RestBiz biz;
