68cd9b5076c127f8480bf334efd24fe84b2fc62b
Kweon <su539@naver.com>
2024-11-08 11:50:18 +0900

    Framework_SpringBoot01

---
+++ StartBoot01/src/main/java/com/boot/hello/StartBoot01Application.java
@@ -0,0 +1,22 @@
+package com.boot.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class StartBoot01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartBoot01Application.class, args);
+	}
+
+	@GetMapping("/index")
+	public String root() {
+		
+		return "test";
+	}
+	
+}
