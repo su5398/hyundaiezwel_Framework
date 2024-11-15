5b724f4d88731019829df1893eef5e7ce8b39b2c
Kweon <su539@naver.com>
2024-11-15 15:16:40 +0900

    Framework_SpringBoot03_Thymeleaf

--- StartBoot03/src/main/java/com/boot/leaf/controller/LeafController.java
+++ StartBoot03/src/main/java/com/boot/leaf/controller/LeafController.java
@@ -1,8 +1,15 @@
 package com.boot.leaf.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.boot.leaf.dto.LeafDto;
 
 @Controller
 public class LeafController {
@@ -21,7 +28,26 @@ public class LeafController {
 	
 	@GetMapping("/expr")
 	public String expression(Model model) {
+		model.addAttribute("name","Thymeleaf");
+		model.addAttribute("dto", new LeafDto("Java",22));
+		
+		List<LeafDto> list = new ArrayList<>();
+		list.add(new LeafDto("StringBoot",33));
+		list.add(new LeafDto("DataBase",7));
+		model.addAttribute("list",list);
 		
 		return "expression";
+	}
+	
+	@GetMapping("/params")
+	public String params(HttpSession session) {
+		session.setAttribute("id","session value");
+		return "params";
+	}
+	
+	@GetMapping("/static")
+	public String staticTest() {
+		
+		return "static";
 	}
 }
