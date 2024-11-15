11e39a2202e3f872dcd38774916925914d8f666d
Kweon <su539@naver.com>
2024-11-15 11:45:43 +0900

    Framework_SpringBoot03_Thymeleaf

---
+++ StartBoot03/src/main/java/com/boot/leaf/controller/LeafController.java
@@ -0,0 +1,27 @@
+package com.boot.leaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LeafController {
+
+	@GetMapping("/hello")
+	public String hello(Model model) {
+		model.addAttribute("name","Thymeleaf");
+		return "hello";
+	}
+	
+	@GetMapping("/util")
+	public String util() {
+		
+		return "utility";
+	}
+	
+	@GetMapping("/expr")
+	public String expression(Model model) {
+		
+		return "expression";
+	}
+}
